c3554a32fe6396d7d2c88509f2f91cdc18d071fe
EMnapolis <m.wachirasak@gmail.com>
2025-05-20 10:45:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,10 +91,7 @@ if menu == "🖥 หน้าต่างทำงาน":
     if "user_id" in st.session_state:
         st.success(f"🎉 คุณเข้าสู่ระบบแล้วในชื่อ {st.session_state['display_name']}")
         status = st.session_state.get("status", "PENDING")
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of b117037 (Update app.py)
         if status != "APPROVED":
             st.warning("⚠️ คุณยังไม่ได้รับสิทธิ์ใช้งาน กรุณารอการอนุมัติ")
         else:
