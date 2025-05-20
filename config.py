4584a41d9b024cff9dbbfae6d85a922fb7598aa9
EMnapolis <m.wachirasak@gmail.com>
2025-05-20 10:44:51 +0700

    changeURI

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 CHANNEL_ID = "1656150106"
 CHANNEL_SECRET = "6f3d52bf5641b5d48bf70a23c8332f86"
-REDIRECT_URI = "https://lineapp-zl4qzvwskg2pb8vhq9ao9f.streamlit.app"
+REDIRECT_URI = "https://lineappappv1-fianqtjgevmrlfgve3ynub.streamlit.app"
 STATE = "my-login-session"  # ค่า state สำหรับความปลอดภัยของ OAuth2
 
 
