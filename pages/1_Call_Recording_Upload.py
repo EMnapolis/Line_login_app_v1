1cdb5d211f18b4a381873ce7a4c4fc9ad0e6964b
EMnapolis <m.wachirasak@gmail.com>
2025-05-20 11:19:10 +0700

    Update 1_Call_Recording_Upload.py

--- pages/1_Call_Recording_Upload.py
+++ pages/1_Call_Recording_Upload.py
@@ -1,10 +1,5 @@
-import os
-import sys
-import streamlit as st
-
-# เพิ่ม path ให้ import ได้
+import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "Call_Recording_Upload"))
-
-# เรียกฟังก์ชันแสดงหน้าหลัก
 from main import render_page
 render_page()
+
