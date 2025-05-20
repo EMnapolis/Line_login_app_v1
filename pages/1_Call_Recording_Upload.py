4231955fb4a851f2d6de8686a660bf755eb00d3e
EMnapolis <m.wachirasak@gmail.com>
2025-05-20 12:00:10 +0700

    delpages

--- pages/1_Call_Recording_Upload.py
+++
@@ -1,7 +0,0 @@
-# 1_Call_Recording_Upload.py
-import os, sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "Call_Recording_Upload"))
-from main import render_call_upload
-render_call_upload()
-
